79a05b2efb26c6b93b2b640268517148d95f7090
Bryan Teng <bteng1226@gmail.com>
2019-06-18 12:27:59 -0500

    Updates

---
+++ precache-manifest.b5e84e08952030b99b43dd2d3707a6e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31875eff46350c086adabbb28f34c9db",
+    "url": "/hello-world/index.html"
+  },
+  {
+    "revision": "6f16c96c774fde12df21",
+    "url": "/hello-world/static/css/main.f9c67ae1.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/hello-world/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "6f16c96c774fde12df21",
+    "url": "/hello-world/static/js/main.6a94f622.chunk.js"
+  },
+  {
+    "revision": "ffb4ea3617005ab7a82a",
+    "url": "/hello-world/static/js/runtime~main.dc31fdf6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello-world/static/media/logo.5d5d9eef.svg"
+  }
+]);
