5c62396ff7178d3b4f9463ce32a37c252b056339
Bryan Teng <bteng1226@gmail.com>
2019-06-21 14:42:43 -0500

    Updates

---
+++ precache-manifest.6ca827aa4c2c881916ea69d0bfe83ef3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e089cf09776b40dcc6d312a47477da15",
+    "url": "/hello-world/index.html"
+  },
+  {
+    "revision": "ac8092ec4e21cb5e7c09",
+    "url": "/hello-world/static/css/main.b668b25c.chunk.css"
+  },
+  {
+    "revision": "beace6d34dde8598f206",
+    "url": "/hello-world/static/js/2.75becca2.chunk.js"
+  },
+  {
+    "revision": "ac8092ec4e21cb5e7c09",
+    "url": "/hello-world/static/js/main.48b49826.chunk.js"
+  },
+  {
+    "revision": "ffb4ea3617005ab7a82a",
+    "url": "/hello-world/static/js/runtime~main.dc31fdf6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hello-world/precache-manifest.faa502c39b27e0e4aadbab07b82528f6.js"
+  "/hello-world/precache-manifest.6ca827aa4c2c881916ea69d0bfe83ef3.js"
 );
 
 self.addEventListener('message', (event) => {
